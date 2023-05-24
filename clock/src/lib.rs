e63c32d61a26653c3dac873135ee61d1ec00a2cb
Daniel Brito <danielbrito@pq.uenf.br>
2023-05-24 08:47:20 -0300

    fix: correct operations to expected behavior

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -8,21 +8,17 @@ pub struct Clock{
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        // unimplemented!("Construct a new Clock from {hours} hours and {minutes} minutes");
-        Clock {hours, minutes}
+        let processed_minutes = (minutes).rem_euclid(60);
+        let processed_hours = (hours + ((minutes).div_euclid(60))).rem_euclid(24);
+        Clock {
+            hours: processed_hours,
+            minutes: processed_minutes
+        }
+
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        // unimplemented!("Add {minutes} minutes to existing Clock time");
-        let total_minutes = self.minutes + minutes;
-        let total_hours = self.hours + (total_minutes / 60);
-        let new_minutes = total_minutes % 60;
-        let new_hours = total_hours % 24;
-
-        Clock {
-            hours: new_hours,
-            minutes: new_minutes
-        }
+        Clock::new(self.hours,self.minutes + minutes)
     }
 }
 
