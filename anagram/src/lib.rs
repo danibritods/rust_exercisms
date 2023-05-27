cc123a43bda10cf44a54eb18c84d69aeebc23fcf
Daniel Brito <danielbrito@pq.uenf.br>
2023-05-26 23:38:35 -0300

    feat: gambiarra to make it work

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
  // unimplemented!(
  // "For the '{word}' word find anagrams among the following words: {possible_anagrams:?}"
  // );
@@ -10,18 +10,18 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a s
  let mut anagrams = HashSet::<&'a str>::new();
 
  for candidate in possible_anagrams {
- if candidate.len() == word_length && 
- candidate.to_lowercase() != word_lower && 
- sort_word(&candidate.to_lowercase()) == word_sorted {
- anagrams.insert(candidate);
- }
+     if candidate.len() == word_length && 
+        candidate.to_lowercase() != word_lower && 
+        sort_word(&candidate.to_lowercase()) == word_sorted {
+         // insert candidate into set without cloning
+         anagrams.insert(candidate);
+     }
  }
  anagrams
-
 }
 
 fn sort_word(word: &str) -> Vec<char> {
- let word_vec: Vec<char> = word.to_lowercase().chars().collect();
+ let mut word_vec: Vec<char> = word.to_lowercase().chars().collect();
  word_vec.sort_unstable();
  word_vec
 }
